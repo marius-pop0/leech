cf7179b97284cf44a60a959dd71f814549aa59ca
mmpop <mmpop@ucalgary.ca>
2017-03-02 17:43:25 -0700

    inputs

--- client.py
+++ client.py
@@ -1,13 +1,29 @@
-import sys
+import sys, socket
 
 command = ""
 filename = ""
 host_port = ""
 cipher = ""
 key = ""
+host= ""
+port = 0
 
 
 
+
+def read():
+    print("reading")
+
+def write():
+    print("writing")
+    s = socket.socket()  # Create a socket object
+
+    s.connect((host, port))
+
+
+    s.send("Hi".encode('ascii'))
+
+    s.close()
 
 
 
@@ -26,3 +42,24 @@ if __name__ == '__main__':
     else:
         print("wrong number of arguments")
         sys.exit(0)
+
+    if(command!="write" and command!="read"):
+        print("Wrong Command Parameter")
+        sys.exit(0)
+
+    host=""
+    port=0
+
+    try:
+        host_port2 = host_port.split(":")
+        host=host_port2[0]
+        port=int(host_port2[1])
+    except ImportError:
+        print("Wrong hostname:port format")
+        sys.exit(0)
+
+    if (cipher != "aes256" and cipher != "aes128" and cipher != "none"):
+        print("Wrong Cipher Parameter")
+        sys.exit(0)
+
+    write()
--- server.py
+++ server.py
@@ -1,18 +1,77 @@
-import sys
+import sys, socketserver, socket
 
-port = 0
+PORT = 0
 key = ""
+command=""
+
+
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    BUFFER_SIZE = 4096
+
+    def handle(self):
+
+        #authorized = True  # authorization flag
+
+
+        data = self.request.recv(self.BUFFER_SIZE)
+        if len(data) == self.BUFFER_SIZE:
+            while 1:
+                try:  # error means no more data
+                    data += self.request.recv(self.BUFFER_SIZE, socket.MSG_DONTWAIT)
+                except:
+                    break
+
+
+
+        #if not authorized:
+            # authorized = (command[0] == password)
+            #if not authorized:
+             #   print(data)
+                ##disconnect if password is not known
+                #self.request.sendall(bytearray(incorrectPw, "UTF-8"))
+              #  sys.exit(1)
+            #else:
+                #self.request.sendall(bytearray(welcome, "UTF-8"))
+
+
+        nonlocal command
+        command = data.decode("utf-8")
+
+        if (command=="write"):
+            print("Writing")
+
+
+
+
+        elif(command=="read"):
+            print("reading")
+
+
+
+
+
+        else:
+            sys.stderr("Invalid Command type Received")
+            sys.exit(1)
+
+
+
 
 
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        command = int(sys.argv[1])
+        PORT = int(sys.argv[1])
     elif len(sys.argv) == 3:
-        command = int(sys.argv[1])
+        PORT = int(sys.argv[1])
         key = sys.argv[2]
 
     else:
         print("wrong number of arguments")
         sys.exit(0)
+
+    server = socketserver.ThreadingTCPServer(("localhost", PORT), MyTCPHandler)
+    server.serve_forever()
