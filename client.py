5b5c16ff124439644a8a3f06eeacb6cf343d9515
hthkwan <herman_fire@hotmail.com>
2017-03-09 23:46:06 -0700

    pushing my progress
    almost all the changes were to server only, client hasn't been updated
    
    server can properly read and send a file w/ SHA-224 hash
    
    server can also receive small files and write them properly, should also
    work with large files > 4MB, but untested
    
    TODO:
    - Client needs to read parts of the file from stdin (in 4MB chunks) and
    send them piece by piece to server
    
    -Handshaking/encryption hasn't been touched, shouldn't be hard though

--- client.py
+++ client.py
@@ -12,54 +12,54 @@ port = 0
 
 
 def read():
-    print("reading")
+	print("reading")
 
 def write():
-    print("writing")
-    s = socket.socket()  # Create a socket object
+	print("writing")
+	
+	s.send("Hi".encode('ascii'))
 
-    s.connect((host, port))
-
-
-    s.send("Hi".encode('ascii'))
-
-    s.close()
+	s.close()
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 5:
-        command = sys.argv[1]
-        filename = sys.argv[2]
-        host_port = sys.argv[3]
-        cipher = sys.argv[4]
-    elif len(sys.argv) == 6:
-        command = sys.argv[1]
-        filename = sys.argv[2]
-        host_port = sys.argv[3]
-        cipher = sys.argv[4]
-        key = sys.argv[5]
-    else:
-        print("wrong number of arguments")
-        sys.exit(0)
+	if len(sys.argv) == 5:
+		command = sys.argv[1]
+		filename = sys.argv[2]
+		host_port = sys.argv[3]
+		cipher = sys.argv[4]
+	elif len(sys.argv) == 6:
+		command = sys.argv[1]
+		filename = sys.argv[2]
+		host_port = sys.argv[3]
+		cipher = sys.argv[4]
+		key = sys.argv[5]
+	else:
+		print("wrong number of arguments")
+		sys.exit(0)
 
-    if(command!="write" and command!="read"):
-        print("Wrong Command Parameter")
-        sys.exit(0)
+	if(command!="write" and command!="read"):
+		print("Wrong Command Parameter")
+		sys.exit(0)
 
-    host=""
-    port=0
+	host=""
+	port=0
 
-    try:
-        host_port2 = host_port.split(":")
-        host=host_port2[0]
-        port=int(host_port2[1])
-    except ImportError:
-        print("Wrong hostname:port format")
-        sys.exit(0)
+	try:
+		host_port2 = host_port.split(":")
+		host=host_port2[0]
+		port=int(host_port2[1])
+	except ImportError:
+		print("Wrong hostname:port format")
+		sys.exit(0)
 
-    if (cipher != "aes256" and cipher != "aes128" and cipher != "none"):
-        print("Wrong Cipher Parameter")
-        sys.exit(0)
+	if (cipher != "aes256" and cipher != "aes128" and cipher != "none"):
+		print("Wrong Cipher Parameter")
+		sys.exit(0)
+	
+	s = socket.socket()  # Create a socket object
 
-    write()
+	s.connect((host, port))
+	
+	write()
--- server.py
+++ server.py
@@ -1,76 +1,124 @@
-import sys, socketserver, socket
+import sys, socketserver, socket, hashlib, binascii
 
 PORT = 0
 key = ""
 
-
+def receiveData(self, socket):
+	BUFFER_SIZE = 4096
+	incoming = self.request.recv(BUFFER_SIZE)
+	if len(incoming) == BUFFER_SIZE:
+		while 1:
+			try:  # error means no more data
+				incoming += self.request.recv(BUFFER_SIZE, socket.MSG_DONTWAIT)
+			except:
+				break
+		return incoming		   
+					
 
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
-    BUFFER_SIZE = 4096
+	BUFFER_SIZE = 4096
+	FILE_BUFSIZE = 4194304
 
-    def handle(self):
+	def handle(self):
+		hostname = self.request.getsockname()
+		peername = self.request.getpeername()
+		print("new connection from " + hostname[0] + " from port " + str(peername[1]))
+		#receiver cipher type and IV here
+		
+		#get the command
+		data = self.request.recv(self.BUFFER_SIZE)
+		if len(data) == self.BUFFER_SIZE:
+			while 1:
+				try:  # error means no more data
+					data += self.request.recv(self.BUFFER_SIZE, socket.MSG_DONTWAIT)
+				except:
+					break
+		
+		command = data.decode("ascii").strip('\n')
+		
+		#both read and write need filename, so do it here
+		data = self.request.recv(self.BUFFER_SIZE)
+		if len(data) == self.BUFFER_SIZE:
+			while 1:
+				try:  # error means no more data
+					data += self.request.recv(self.BUFFER_SIZE, socket.MSG_DONTWAIT)
+				except:
+					break
+		filename = data.decode("ascii").strip('\n')
+		print("command: " + command) #log
+		
+		if (command == "write"):
+			print("Writing")
+			
+			#receive file
+			fileData = b''
+			
+			while 1:
+				data = self.request.recv(self.FILE_BUFSIZE)
+				if data:
+					fileData += data
+					if len(data) < self.FILE_BUFSIZE:
+						break
+				else:
+					break
+			#encrypt fileData here
+			try:
+				with open(filename, "wb") as f:
+					f.write(fileData)
+				print("file write success")
+			except:
+				print("file write failed")
+				return 1
+				
+			print("ok")
+			#send acknowledgement back to client
+			self.request.sendall("ok".encode("ascii"))
+			
+		elif (command == "read"):
+			print("reading")
+			#read data from file
+			
+			try:
+				with open(filename, "rb") as f:
+					readData = f.read()
+				logOutput = readData.decode("ascii")
+				print(logOutput)
+			except:
+				print ("error reading file")
+				self.request.sendall(b"DNE")
 
-        #authorized = True  # authorization flag
+			
+			#calculate SHA-1 hash for file
+			m = hashlib.sha224()
+			m.update(readData)
+			hash = m.digest()
+			
+			print(binascii.hexlify(hash))
+			
+			#send file and hash back to client
+			self.request.sendall(readData)
+			self.request.sendall(hash)
+			
+		else:
+			print("Invalid Command Type Received: " + command)
+		self.request.close()
+		print("closing connection")
+		
 
-
-        data = self.request.recv(self.BUFFER_SIZE)
-        if len(data) == self.BUFFER_SIZE:
-            while 1:
-                try:  # error means no more data
-                    data += self.request.recv(self.BUFFER_SIZE, socket.MSG_DONTWAIT)
-                except:
-                    break
-
-
-
-        #if not authorized:
-            # authorized = (command[0] == password)
-            #if not authorized:
-             #   print(data)
-                ##disconnect if password is not known
-                #self.request.sendall(bytearray(incorrectPw, "UTF-8"))
-              #  sys.exit(1)
-            #else:
-                #self.request.sendall(bytearray(welcome, "UTF-8"))
-
-
-
-        command = data.decode("utf-8")
-
-        if (command=="write"):
-            print("Writing")
-
-
-
-
-        elif(command=="read"):
-            print("reading")
-
-
-
-
-
-        else:
-            print("Invalid Command Type Received")
-
-
-
-
-
-
-
+		
+#arguents: python3 server.py port [key]
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        PORT = int(sys.argv[1])
-    elif len(sys.argv) == 3:
-        PORT = int(sys.argv[1])
-        key = sys.argv[2]
+	if len(sys.argv) == 2:
+		PORT = int(sys.argv[1])
+	elif len(sys.argv) == 3:
+		PORT = int(sys.argv[1])
+		key = sys.argv[2]
 
-    else:
-        print("wrong number of arguments")
-        sys.exit(0)
+	else:
+		print("Incorrect usage: server.py port [key]")
+		sys.exit(0)
 
-    server = socketserver.ThreadingTCPServer(("localhost", PORT), MyTCPHandler)
-    server.serve_forever()
+	server = socketserver.ThreadingTCPServer(("localhost", PORT), MyTCPHandler)
+	server.serve_forever()
